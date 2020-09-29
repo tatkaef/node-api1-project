1282ea1a476f97dc711c0466ab954215db1edb26
Tatiana Efimova <tatka_ef@mail.ru>
2020-09-29 16:04:50 -0700

    MVP

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get("/", (req, res) => {
 
 let users = [
   {
-    id: "a_unique_id", // hint: use the shortid npm package to generate it
+    id: 1, // hint: use the shortid npm package to generate it
     name: "Jane Doe", // String, required
     bio: "Not Tarzan's Wife, another Jane", // String, required
   },
@@ -30,13 +30,19 @@ server.get("/api/users", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id); //???
   const user = users.find((user) => user.id === id);
-  res.status(200).json(user);
-  res
-    .status(404)
-    .json({ message: "The user with the specified ID does not exist." });
-  res
-    .status(500)
-    .json({ errorMessage: "The user information could not be retrieved." });
+  if (!user) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else {
+    try {
+      res.status(200).json(user);
+    } catch {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    }
+  }
 });
 
 //POST /api/users
@@ -49,10 +55,66 @@ server.post("/api/users", function (req, res) {
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
   } else {
-    res.status(201).json({ data: users });
-    res.status(500).json({
-      errorMessage: "There was an error while saving the user to the database",
-    });
+    try {
+      res.status(201).json({ data: users });
+    } catch {
+      res.status(500).json({
+        errorMessage:
+          "There was an error while saving the user to the database",
+      });
+    }
+  }
+});
+
+//PUT /api/users/:id
+server.put("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const edited = req.body;
+
+  let userToEdit = users.find((u) => u.id === id);
+
+  if (!edited.name || !edited.bio) {
+    //respond with HTTP status code 400 (Bad Request).
+    //return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
+    //If there's an error when updating the user:
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+    //If the user with the specified id is not found:
+  } else if (!userToEdit) {
+    //respond with HTTP status code 404 (Not Found).
+    //return the following JSON object: { message: "The user with the specified ID does not exist." }.
+    //If the request body is missing the name or bio property:
+    res
+      .status(404)
+      .json({ errorMessage: "The user with the specified ID does not exist." });
+  } else if (userToEdit) {
+    Object.assign(userToEdit, edited);
+    //If the user is found and the new information is valid:
+    //update the user document in the database using the new information sent in the request body.
+    //respond with HTTP status code 200 (OK).
+    //return the newly updated user document.
+    res.status(200).json(userToEdit);
+  } else {
+    //respond with HTTP status code 500.
+    //return the following JSON object: { errorMessage: "The user information could not be modified." }.
+    res
+      .status(500)
+      .json({ errorMessage: "The user information could not be modified" });
+  }
+});
+
+//DELETE /api/users/:id
+server.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const deleted = users.find((u) => u.id === id);
+  if (!deleted) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else {
+    users = users.filter((u) => u.id !== id);
+    res.status(200).json({ data: users });
   }
 });
 
